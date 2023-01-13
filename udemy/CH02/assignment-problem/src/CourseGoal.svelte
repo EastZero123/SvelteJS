ce6d29b81db0b7f99b81736191d2526afae001cf
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2023-01-13 17:00:58 +0900

    CH02 finish

---
+++ udemy/CH02/assignment-problem/src/CourseGoal.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let doHighlight
+  $: doHighlight = goal.includes("!")
+</script>
+
+<h1 class:highlight={doHighlight}>{goal}</h1>
+
+<style>
+  .highlight {
+    color: red;
+  }
+</style>
---
+++ udemy/CH02/new/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import ContactCard from "./ContactCard.svelte";
+
+	let name = 'Max';
+	let title = ""
+	let image = ""
+	let description = ""
+	let age = 30;
+
+	$: uppercaseName = name.toUpperCase()
+
+	function incrementAge() {
+		age += 1;
+	}
+
+	function changeName() {
+		name = "CHOI"
+	}
+
+	function nameInput(event) {
+		const enteredValue = event.target.value
+		name = enteredValue
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<!-- <button on:click="{changeName}">Change Name</button>
+<input type="text" value="{name}" on:input="{nameInput}"> -->
+<input type="text" bind:value={name}/>
+<input type="text" bind:value={title}/>
+<input type="text" bind:value={image}/>
+<textarea rows="3" bind:value={description}></textarea>
+
+<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
